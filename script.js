afa12a51c763d094e6ddd38c97c1b6bea3e97526
Grace Grumo <90876119+grcgrm@users.noreply.github.com>
2023-06-01 17:20:04 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// Custom JavaScript
+
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
