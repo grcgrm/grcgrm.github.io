be615e044acaa045cc72b8be2caa1b4092cae5dc
Grace Grumo <90876119+grcgrm@users.noreply.github.com>
2023-06-06 17:24:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,13 +11,16 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   });
 });
 
-// Calculate the years and months of experience
-var startDate = new Date("2022-03-01");
-var currentDate = new Date();
-var totalMonths = (currentDate.getFullYear() - startDate.getFullYear()) * 12 + (currentDate.getMonth() - startDate.getMonth());
-var years = Math.floor(totalMonths / 12);
-var months = totalMonths % 12;
+document.addEventListener("DOMContentLoaded", function() {
+  // Calculate the years and months of experience
+  var startDate = new Date("2022-03-01");
+  var currentDate = new Date();
+  var totalMonths = (currentDate.getFullYear() - startDate.getFullYear()) * 12 + (currentDate.getMonth() - startDate.getMonth());
+  var years = Math.floor(totalMonths / 12);
+  var months = totalMonths % 12;
 
-// Update the HTML elements with the calculated values
-document.getElementById("years").innerText = years + " year";
-document.getElementById("months").innerText = months + " months";
+  // Update the HTML elements with the calculated values
+  document.getElementById("years").innerText = years + " years";
+  document.getElementById("months").innerText = months + " months";
+});
+
